077258af571b733e7c4b96635b52f272a659ac6d
ethanjd23 <ethanjd23@gmail.com>
2021-03-05 15:20:00 -0600

    required part finished

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use((req, res, next) => {
+    console.log(req.originalUrl);
+    next();
+})
+
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res) => {
     res.send("Hello from the server side...");
