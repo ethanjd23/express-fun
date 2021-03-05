6c98a915b3e5365177ac89f65531effd8c1c21d8
ethanjd23 <ethanjd23@gmail.com>
2021-03-05 16:17:47 -0600

    Finished

--- server/server.js
+++ server/server.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+let submissionsArray = [];
 
-app.use((req, res, next) => {
-    console.log(req.originalUrl);
-    next();
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.post("/formsubmissions", (req, res) => {
+    submissionsArray.push({ email: req.body.email, name: req.body.name })
 
-app.get('/', (req, res) => {
-    res.send("Hello from the server side...");
-})
+  fs.writeFileSync(
+    path.join(__dirname, "../public/submissions.json"),
+    JSON.stringify(submissionsArray),
+    (err) => {
+      if (err) throw err;
+    }
+  );
+  res.redirect("/");
+//   res.sendFile(path.join(__dirname, "../public/submissions.json"));
+});
+
+
+// app.use((req, res, next) => {
+//     console.log(req.originalUrl);
+//     next();
+// })
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.send("Hello from the server side...");
+});
 
 app.listen(3000);
